dff4fe57092ee393f0be500cf6680ad2ba04ff8e
arzzcom <arzz@arzz.com>
2023-03-31 20:25:34 +0900

    모듈 환경설정을 위한 폼 객체 정의

---
+++ admin/scripts/AttachmentAdmin.js
@@ -0,0 +1,70 @@
+/**
+ * 이 파일은 아이모듈 관리자모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 관리자 UI 이벤트를 관리하는 클래스를 정의한다.
+ *
+ * @file /modules/attachment/admin/scripts/AttachmentAdmin.ts
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2023. 3. 28.
+ */
+var modules;
+(function (modules) {
+    let attachment;
+    (function (attachment) {
+        class AttachmentAdmin extends Admin.Interface {
+            /**
+             * 모듈 환경설정 폼을 가져온다.
+             */
+            async getConfigsForm() {
+                return new Admin.Form.Panel({
+                    items: [
+                        new Admin.Form.FieldSet({
+                            title: (await this.getText('admin/configs/default')),
+                            items: [
+                                new Admin.Form.Field.Template({
+                                    label: (await this.getText('admin/configs/template')),
+                                    name: 'template',
+                                    targetType: this.getType(),
+                                    targetName: this.getName(),
+                                }),
+                            ],
+                        }),
+                        new Admin.Form.FieldSet({
+                            title: (await this.getText('admin/configs/limits')),
+                            items: [
+                                new Admin.Form.Field.Container({
+                                    label: (await this.getText('admin/configs/max_file_size')),
+                                    items: [
+                                        new Admin.Form.Field.Number({
+                                            name: 'max_file_size',
+                                            width: 80,
+                                        }),
+                                        new Admin.Form.Field.Display({
+                                            value: 'MB',
+                                        }),
+                                    ],
+                                    helpText: (await this.getText('admin/configs/max_file_size_help')),
+                                }),
+                                new Admin.Form.Field.Container({
+                                    label: (await this.getText('admin/configs/max_upload_size')),
+                                    items: [
+                                        new Admin.Form.Field.Number({
+                                            name: 'max_upload_size',
+                                            width: 80,
+                                        }),
+                                        new Admin.Form.Field.Display({
+                                            value: 'MB',
+                                        }),
+                                    ],
+                                    helpText: (await this.getText('admin/configs/max_upload_size_help')),
+                                }),
+                            ],
+                        }),
+                    ],
+                });
+            }
+        }
+        attachment.AttachmentAdmin = AttachmentAdmin;
+    })(attachment = modules.attachment || (modules.attachment = {}));
+})(modules || (modules = {}));
---
+++ admin/scripts/AttachmentAdmin.ts
@@ -0,0 +1,67 @@
+/**
+ * 이 파일은 아이모듈 관리자모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 관리자 UI 이벤트를 관리하는 클래스를 정의한다.
+ *
+ * @file /modules/attachment/admin/scripts/AttachmentAdmin.ts
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2023. 3. 28.
+ */
+namespace modules {
+    export namespace attachment {
+        export class AttachmentAdmin extends Admin.Interface {
+            /**
+             * 모듈 환경설정 폼을 가져온다.
+             */
+            async getConfigsForm(): Promise<Admin.Form.Panel> {
+                return new Admin.Form.Panel({
+                    items: [
+                        new Admin.Form.FieldSet({
+                            title: (await this.getText('admin/configs/default')) as string,
+                            items: [
+                                new Admin.Form.Field.Template({
+                                    label: (await this.getText('admin/configs/template')) as string,
+                                    name: 'template',
+                                    targetType: this.getType(),
+                                    targetName: this.getName(),
+                                }),
+                            ],
+                        }),
+                        new Admin.Form.FieldSet({
+                            title: (await this.getText('admin/configs/limits')) as string,
+                            items: [
+                                new Admin.Form.Field.Container({
+                                    label: (await this.getText('admin/configs/max_file_size')) as string,
+                                    items: [
+                                        new Admin.Form.Field.Number({
+                                            name: 'max_file_size',
+                                            width: 80,
+                                        }),
+                                        new Admin.Form.Field.Display({
+                                            value: 'MB',
+                                        }),
+                                    ],
+                                    helpText: (await this.getText('admin/configs/max_file_size_help')) as string,
+                                }),
+                                new Admin.Form.Field.Container({
+                                    label: (await this.getText('admin/configs/max_upload_size')) as string,
+                                    items: [
+                                        new Admin.Form.Field.Number({
+                                            name: 'max_upload_size',
+                                            width: 80,
+                                        }),
+                                        new Admin.Form.Field.Display({
+                                            value: 'MB',
+                                        }),
+                                    ],
+                                    helpText: (await this.getText('admin/configs/max_upload_size_help')) as string,
+                                }),
+                            ],
+                        }),
+                    ],
+                });
+            }
+        }
+    }
+}
